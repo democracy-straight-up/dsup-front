fef4a125439a575522f13da65f61bb5f607a0364
Zhongyi Lu <zhongyi_lu@outlook.com>
2024-03-18 14:56:52 -0700

    feat: update `GenPass `function

--- src/components/Claim-Your-Seat.jsx
+++ src/components/Claim-Your-Seat.jsx
@@ -7,32 +7,27 @@ import { Eye, EyeSlash } from 'react-bootstrap-icons';
 import Tooltip from 'react-bootstrap/Tooltip';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 
-export const GenPass = function (length) {
-  const charset =
-    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
-  let password = "";
-  let hasLower = false,
-    hasUpper = false,
-    hasNumber = false,
-    hasSpecial = false;
-
-  while (
-    !hasLower ||
-    !hasUpper ||
-    !hasNumber ||
-    !hasSpecial ||
-    password.length < length
-  ) {
-    const char = charset.charAt(Math.floor(Math.random() * charset.length));
-    password += char;
-
-    hasLower = hasLower || /[a-z]/.test(char);
-    hasUpper = hasUpper || /[A-Z]/.test(char);
-    hasNumber = hasNumber || /[0-9]/.test(char);
-    hasSpecial = hasSpecial || /[!@#$%^&*()]/.test(char);
-  }
-
-  return password;
+export const GenPass = function(length) {
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+    const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+    const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numbers = "0123456789";
+    const specialChars = "!@#$%^&*()";
+    let password = [];
+  
+    password.push(lowerCase.charAt(Math.floor(Math.random() * lowerCase.length)));
+    password.push(upperCase.charAt(Math.floor(Math.random() * upperCase.length)));
+    password.push(numbers.charAt(Math.floor(Math.random() * numbers.length)));
+    password.push(specialChars.charAt(Math.floor(Math.random() * specialChars.length)));
+  
+    for (let i = 4; i < length; i++) {
+        password.push(charset.charAt(Math.floor(Math.random() * charset.length)));
+    }
+  
+    // shuffle password
+    password = password.sort(() => Math.random() - 0.5);
+  
+    return password.join('');
 };
 
 function ClaimYourSeat() {
