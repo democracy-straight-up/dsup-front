75f8c8607eea74e179e63683bbf133d4bc91b944
Zaman Ehsani <zamanehsani@gmail.com>
2025-04-21 10:58:51 +0400

    Moda (#57)
    * fixed the page of Enter-The-Floor-Page
    
    * buf fixes and Circle improvements
    
    * finished the improvement on circle, housekeeping page and more
    
    * fixed the status_messages on Circle Housekeeping page
    
    * added the Second-Link and it's associated files, pages, and components
    
    * bug fixes and updates

--- src/components/sec_del/candidate.jsx
+++ src/components/sec_del/candidate.jsx
@@ -70,9 +70,7 @@ export default function Candidate({
 
   return (
     <tr>
-      <td>
-        {index + 1} - {candidate.id}
-      </td>
+      <td>{index + 1}</td>
       <td>{candidate?.user?.users?.legalName}</td>
 
       {Iam_member || Iam_delegate ? (
