abd3662644b1de818e13f8758c8ac8bfd64f8573
Zaman Ehsani <zamanehsani@gmail.com>
2025-10-12 11:25:02 +0400

    updates on Circle voter card

--- src/components/voter_page_components/circle_card.jsx
+++ src/components/voter_page_components/circle_card.jsx
@@ -190,22 +190,26 @@ export default function CircleCard() {
                 </div>
 
                 <div className="row">
-                  {/* {AuthUser?.users.userType === "U1D1" ? ( */}
-                  <div className="d-flex flex-sm-column flex-md-row justify-content-evenly mt-4 ">
-                    {circleInfo?.status && (
-                      <>
-                        <Link to="#" onClick={handleCreate} className="py-1 text-nowrap text-dark">
-                          Create F-Link
-                        </Link>
-                        {}
-                        <Link to="/join-sec-del" className="py-1 text-nowrap  text-dark">
-                          Join F-Link
-                        </Link>
-                      </>
-                    )}
-                  </div>
-                  {/* ) : (<div style={{ height: "3.5rem", display: "block" }}></div>
-                  )} */}
+                  {AuthUser?.users.userType === "U1D1" ? (
+                    <div className="d-flex flex-sm-column flex-md-row justify-content-evenly mt-4 ">
+                      {circleInfo?.status && (
+                        <>
+                          <Link
+                            to="#"
+                            onClick={handleCreate}
+                            className="py-1 text-nowrap text-dark">
+                            Create F-Link
+                          </Link>
+                          {}
+                          <Link to="/join-sec-del" className="py-1 text-nowrap  text-dark">
+                            Join F-Link
+                          </Link>
+                        </>
+                      )}
+                    </div>
+                  ) : (
+                    <div style={{ height: "3.3rem", display: "block" }}></div>
+                  )}
                 </div>
               </>
             ) : (
